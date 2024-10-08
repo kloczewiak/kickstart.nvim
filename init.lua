a35d598eff269a5d3e01018f1e5deac2ed49e739
MafinBoy <kloczewiak.filip@gmail.com>
2024-10-09 01:38:11 +0200

    Add vim to lua globals

--- init.lua
+++ init.lua
@@ -632,8 +632,11 @@ require('lazy').setup({
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                -- disable = { 'missing-fields' }
+                globals = { 'vim' },
+              },
             },
           },
         },
