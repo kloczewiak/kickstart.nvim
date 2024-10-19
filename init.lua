c0af118bda1ecf2afb6bd10b47f87f6b8c4d0bdb
Filip Kłoczewiak <kloczewiak.filip@gmail.com>
2024-10-19 22:31:54 +0200

    Add abort binding to cmp

--- init.lua
+++ init.lua
@@ -637,6 +637,8 @@ require('lazy').setup({
             end
           end, { 'i', 's' }),
 
+          ['<Esc>'] = cmp.mapping.abort(),
+
           -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         },
