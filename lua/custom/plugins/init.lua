633653dd40fecc9929ef095f6f8ab831f51b28f8
MafinBoy <kloczewiak.filip@gmail.com>
2024-11-06 19:46:49 +0100

    Add markdown preview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -63,4 +63,14 @@ return {
       require('lsp-file-operations').setup()
     end,
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.keymap.set('n', '<leader>tm', ':MarkdownPreviewToggle<CR>', { desc = '[T]oggle [M]arkdown Preview' })
+    end,
+    ft = { 'markdown' },
+  },
 }
