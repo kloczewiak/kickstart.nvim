93908170a261703a4f234ea1e268107a87c6b632
Filip Kłoczewiak <kloczewiak.filip@gmail.com>
2025-03-23 01:44:38 +0100

    Add lazygit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -99,6 +99,34 @@ return {
       -- because Control forwardslash on windows reports as <C-_> for some reason
       open_mapping = { '<C-_>', '<C-/>' },
     },
+    init = function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new {
+        cmd = 'lazygit',
+        dir = 'git_dir',
+        hidden = true,
+        direction = 'float',
+        float_opts = {
+          border = 'curved',
+          width = function()
+            return math.floor(vim.o.columns * 0.8)
+          end,
+          height = function()
+            return math.floor(vim.o.lines * 0.8)
+          end,
+        },
+        on_open = function(term)
+          vim.bo[term.bufnr].modifiable = false
+          vim.bo[term.bufnr].readonly = true
+          vim.cmd 'startinsert!'
+          vim.keymap.set('t', '<esc>', '<esc>', { buffer = term.bufnr, nowait = true })
+        end,
+      }
+
+      vim.keymap.set('n', '<leader>g', function()
+        lazygit:toggle()
+      end, { desc = 'Toggle Lazy[G]it' })
+    end,
   },
   {
     'antosha417/nvim-lsp-file-operations',
